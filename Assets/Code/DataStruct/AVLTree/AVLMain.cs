522f5f42a3f471a011d6deeb21d37ad9aa79328b
Monk <as13679019999@qq.com>
2018-11-06 20:58:39 +0800

    提交avl二叉树

---
+++ Assets/Code/DataStruct/AVLTree/AVLMain.cs
@@ -0,0 +1,49 @@
+﻿using AVL;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AVLMain : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        ////右旋
+        //Debug.Log("tree ##################");
+        //AVLTree<int> tree = new AVLTree<int>();
+        //tree.Insert(20);
+        //tree.Insert(15);
+        //tree.Insert(30);
+        //tree.Insert(13);
+        //tree.Insert(17);
+        //tree.Insert(8);
+        //tree.LevelTraverse();
+        ////先左旋再右旋
+        //Debug.Log("tree1 ##################");
+        //AVLTree<int> tree1 = new AVLTree<int>();
+        //tree1.Insert(20);
+        //tree1.Insert(15);
+        //tree1.Insert(30);
+        //tree1.Insert(12);
+        //tree1.Insert(18);
+        //tree1.Insert(16);
+        //tree1.Insert(7);
+        //tree1.Insert(5);
+        //tree1.LevelTraverse();
+
+        //先右旋再左旋
+        Debug.Log("tree2 ##################");
+        AVLTree<int> tree2 = new AVLTree<int>();
+        tree2.Insert(20);
+        tree2.Insert(15);
+        tree2.Insert(30);
+        tree2.Insert(25);
+        tree2.Insert(36);
+        tree2.Insert(37);
+        tree2.LevelTraverse();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Code/DataStruct/AVLTree/AVLTree.cs
+++ Assets/Code/DataStruct/AVLTree/AVLTree.cs
@@ -1,39 +1,200 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace AVL
 {
-    public class TreeNode<T>
+    public class TreeNode<T> where T : IComparable<T>, IEquatable<T>
     {
-        public int height;
+        public int mHeight;
         public T mData;
         public TreeNode<T> mLeftNode;
         public TreeNode<T> mRightNode;
     }
 
-    public class AVLTree
+    public class AVLTree<T> where T : IComparable<T>, IEquatable<T>
     {
-        public void Balance()
-        {
+        private TreeNode<T>[] mPath = new TreeNode<T>[32];
+        private TreeNode<T> mRoot;
 
+        public void Insert(T data)
+        {
+            Array.Clear(mPath, 0, mPath.Length);
+            if (mRoot == null)
+            {
+                mRoot = new TreeNode<T>();
+                mRoot.mHeight = 0;
+                mRoot.mData = data;
+                return;
+            }
+
+            int index = 0;
+            TreeNode<T> curNode = mRoot;
+            TreeNode<T> curParentNode = null;
+            while(true)
+            {
+                mPath[index] = curNode;
+                curParentNode = curNode;
+                if (data.CompareTo(curNode.mData) < 0)
+                {
+                    curNode = curNode.mLeftNode;
+                    if (curNode == null)
+                    {
+                        curParentNode.mLeftNode = new TreeNode<T>();
+                        curParentNode.mLeftNode.mHeight = 0;
+                        curParentNode.mLeftNode.mData = data;
+                        break;
+                    }
+                }
+                else
+                {
+                    curNode = curNode.mRightNode;
+                    if (curNode == null)
+                    {
+                        curParentNode.mRightNode = new TreeNode<T>();
+                        curParentNode.mRightNode.mHeight = 0;
+                        curParentNode.mRightNode.mData = data;
+                        break;
+                    }
+                }
+                index++;
+            }
+
+            while(index >= 0)
+            {
+                TreeNode<T> pathCurNode = mPath[index];
+                pathCurNode.mHeight = GetHeight(pathCurNode);
+                TreeNode<T> newNode = Balance(pathCurNode);
+                if (newNode != null)
+                {
+                    if(index - 1 >= 0)
+                    {
+                        if (newNode.mData.CompareTo(mPath[index - 1].mData) < 0)
+                        {
+                            mPath[index - 1].mLeftNode = newNode;
+                        }
+                        else
+                        {
+                            mPath[index - 1].mRightNode = newNode;
+                        }
+                    }
+                    else
+                    {
+                        mRoot = newNode;
+                    }
+                }
+                index--;
+            }
         }
 
-        public void LeftRotate()
+        private TreeNode<T> Balance(TreeNode<T> curNode)
         {
+            TreeNode<T> newNode = null;
+            int bf = GetBF(curNode);
+            if(bf == 2)
+            {
+                int leftBF = GetBF(curNode.mLeftNode);
+                if(leftBF == 1)
+                {
+                    newNode = RightRotate(curNode);
+                }
+                else
+                {
+                    newNode = LeftRightRotate(curNode);
+                }
+            }
+            else if(bf == -2)
+            {
+                int rightBF = GetBF(curNode.mRightNode);
+                if (rightBF == 1)
+                {
+                    newNode = RightLeftRotate(curNode);
+                }
+                else
+                {
+                    newNode = LeftRotate(curNode);
+                }
+            }
 
+            return newNode;
+        }
+        
+        private TreeNode<T> LeftRotate(TreeNode<T> root)
+        {
+            TreeNode<T> newRoot = root.mRightNode;
+            root.mRightNode = newRoot.mLeftNode;
+            newRoot.mLeftNode = root;
+
+
+            return newRoot;
         }
 
-        public void RightRotate()
+        private TreeNode<T> RightRotate(TreeNode<T> root)
         {
-
+            TreeNode<T> newRoot = root.mLeftNode;
+            root.mLeftNode = newRoot.mRightNode;
+            newRoot.mRightNode = root;
+            return newRoot;
         }
 
-
-
-        public void Insert()
+        private TreeNode<T> LeftRightRotate(TreeNode<T> root)
         {
+            root.mLeftNode = LeftRotate(root.mLeftNode);
+            return RightRotate(root);
+        }
 
+        private TreeNode<T> RightLeftRotate(TreeNode<T> root)
+        {
+            root.mRightNode = RightRotate(root.mRightNode);
+            return LeftRotate(root);
+        }
+
+        private int GetBF(TreeNode<T> node)
+        {
+            int leftHeight = GetHeight(node.mLeftNode);
+            int rightHeight = GetHeight(node.mRightNode);
+            return leftHeight - rightHeight;
+        }
+
+        private int GetHeight(TreeNode<T> node)
+        {
+            if(node == null)
+            {
+                return -1;
+            }
+            else
+            {
+                int leftHeight = -1;
+                if(node.mLeftNode != null)
+                {
+                    leftHeight = node.mLeftNode.mHeight;
+                }
+
+                int rightHeight = -1;
+                if (node.mRightNode != null)
+                {
+                    rightHeight = node.mRightNode.mHeight;
+                }
+
+                return Mathf.Max(leftHeight, rightHeight) + 1;
+            }
+        }
+
+        public void LevelTraverse()
+        {
+            Queue<TreeNode<T>> queue = new Queue<TreeNode<T>>();
+            queue.Enqueue(mRoot);
+            while(queue.Count > 0)
+            {
+                TreeNode<T> node = queue.Dequeue();
+                if(node != null)
+                {
+                    Debug.Log(node.mData);
+                    queue.Enqueue(node.mLeftNode);
+                    queue.Enqueue(node.mRightNode);
+                }
+            }
         }
 
         public void Delete()
