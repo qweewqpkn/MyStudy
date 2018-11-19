20c494232cdfc6acf2a7231251be6c24a21f2cd4
Monk <as13679019999@qq.com>
2018-11-19 20:58:23 +0800

    提交四叉树的逻辑

--- Assets/Code/SpacePartition/QuadTree/QuadMain.cs
+++ Assets/Code/SpacePartition/QuadTree/QuadMain.cs
@@ -1,8 +1,36 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 class QuadMain : MonoBehaviour
 {
+    QuadTreeNode<QuadTreeData> root;
 
+    void Start()
+    {
+        Rect rect = new Rect(0, 0, 100, 100);
+        root = new QuadTreeNode<QuadTreeData>(0, rect);
+        for(int i = 0; i < 100; i++)
+        {
+            float x = Random.Range(0, 100);
+            float y = Random.Range(0, 100);
+            float width = Random.Range(1, 10);
+            float height = Random.Range(1, 10);
+            QuadTreeData data = new QuadTreeData();
+            data.mRect = new Rect(x, y, width, height);
+            root.Insert(data);
+        }
 
+        List<QuadTreeData> list = new List<QuadTreeData>();
+        root.Retrieve(ref list, new Rect(0, 0, 5, 5));
+        Debug.Log(list.Count);
+    }
+
+    void OnDrawGizmos()
+    {
+        if(root != null)
+        {
+            root.DrawAll();
+        }  
+    }
 }
 
--- Assets/Code/SpacePartition/QuadTree/QuadTree.cs
+++ Assets/Code/SpacePartition/QuadTree/QuadTree.cs
@@ -6,19 +6,19 @@ using UnityEngine;
 //1.使用四叉树来管理场景的节点
 //2.快速定位场景某个点指定距离内的所有物体
 
-public class Bound
+public class QuadTreeData
 {
     public Rect mRect;
 }
 
-public class QuadTreeNode<T> where T : Bound
+public class QuadTreeNode<T> where T : QuadTreeData
 {
-    private const int MAX_DATA = 10;
+    private const int MAX_DATA = 1;
     private const int MAX_LEVEL = 5;
 
     private int mLevel;
     private List<T> mDataList;
-    private Rect mRect;
+    public Rect mRect;
     private QuadTreeNode<T>[] mNodeList;
     
     public QuadTreeNode(int level, Rect rect)
@@ -26,43 +26,54 @@ public class QuadTreeNode<T> where T : Bound
         mLevel = level;
         mRect = rect;
         mDataList = new List<T>();
-        mNodeList = new QuadTreeNode<T>[4];
+        mNodeList = null;
     }
 
     public void Clear()
     {
         mDataList.Clear();
-        for(int i = 0; i < mNodeList.Length; i++)
+        if(mNodeList != null)
         {
-            mNodeList[i].Clear();
+            for (int i = 0; i < mNodeList.Length; i++)
+            {
+                mNodeList[i].Clear();
+            }
         }
     }
 
-    //  0 | 1
-    // -------
-    //  2 | 3
+    /*
+      3  |  2
+    ------------ midY
+      0  |  1
+        midX
+    */
     private QuadTreeNode<T> GetNode(Rect rect)
     {
+        if(mNodeList == null)
+        {
+            return null;
+        }
+
         float midX = mRect.xMin + mRect.width / 2.0f;
         float midY = mRect.yMin + mRect.height / 2.0f;
 
         int index = -1;
-        if (rect.xMin > mRect.xMin && rect.xMax < midX && rect.yMin > mRect.yMin && rect.yMax < midY)
+        if (rect.xMin >= mRect.xMin && rect.xMax <= midX && rect.yMin >= mRect.yMin && rect.yMax <= midY)
         {
             index = 0;
         }
 
-        if (rect.xMin > midX && rect.xMax < mRect.xMax && rect.yMin > mRect.yMin && rect.yMax < midY)
+        if (rect.xMin >= midX && rect.xMax <= mRect.xMax && rect.yMin >= mRect.yMin && rect.yMax <= midY)
         {
             index = 1;
         }
 
-        if (rect.xMin > mRect.xMin && rect.xMax < midX && rect.yMin > midY && rect.yMax < mRect.yMax)
+        if (rect.xMin >= midX && rect.xMax <= mRect.xMax && rect.yMin >= midY && rect.yMax <= mRect.yMax)
         {
             index = 2;
         }
 
-        if (rect.xMin >  midX && rect.xMax < mRect.xMax && rect.yMin > midY && rect.yMax < mRect.yMax)
+        if (rect.xMin >= mRect.xMin && rect.xMax <= midX && rect.yMin >= midY && rect.yMax <= mRect.yMax)
         {
             index = 3;
         }
@@ -77,32 +88,38 @@ public class QuadTreeNode<T> where T : Bound
         }
     }
 
-    private void GetNodeAllData(List<T> list, QuadTreeNode<T> node)
+    private void GetNodeAllData(ref List<T> list)
     {
-        if(node != null)
+        list.AddRange(mDataList);
+        if(mNodeList != null)
         {
-            list.AddRange(node.mDataList);
-        }
-        else
-        {
-            return;
-        }
-
-        for(int i = 0; i < 4; i++)
-        {
-            GetNodeAllData(list, node.mNodeList[i]);
+            for (int i = 0; i < mNodeList.Length; i++)
+            {
+                mNodeList[i].GetNodeAllData(ref list);
+            }
         }
     }
 
+    /*
+       3  |  2
+     ------------
+       0  |  1
+     */
     private void Split()
     {
-        float midX = mRect.xMin + mRect.width / 2.0f;
-        float midY = mRect.yMin + mRect.height / 2.0f;
+        if(mNodeList == null)
+        {
+            float width = mRect.width / 2.0f;
+            float height = mRect.height / 2.0f;
+            float midX = mRect.xMin + width;
+            float midY = mRect.yMin + height;
 
-        mNodeList[0] = new QuadTreeNode<T>(mLevel + 1, new Rect(mRect.x, mRect.y, midX, midY));
-        mNodeList[1] = new QuadTreeNode<T>(mLevel + 1, new Rect(midX, mRect.y, mRect.xMax, midY));
-        mNodeList[2] = new QuadTreeNode<T>(mLevel + 1, new Rect(mRect.x, midY, midX, mRect.yMax));
-        mNodeList[3] = new QuadTreeNode<T>(mLevel + 1, new Rect(midX, midY, mRect.xMax, mRect.yMax));
+            mNodeList = new QuadTreeNode<T>[4];
+            mNodeList[0] = new QuadTreeNode<T>(mLevel + 1, new Rect(mRect.x, mRect.y, width, height));
+            mNodeList[1] = new QuadTreeNode<T>(mLevel + 1, new Rect(midX, mRect.y, width, height));
+            mNodeList[2] = new QuadTreeNode<T>(mLevel + 1, new Rect(midX, midY, width, height));
+            mNodeList[3] = new QuadTreeNode<T>(mLevel + 1, new Rect(mRect.x, midY, width, height));
+        }
     }
 
     public void Insert(T data)
@@ -120,10 +137,9 @@ public class QuadTreeNode<T> where T : Bound
         if(mDataList.Count > MAX_DATA && mLevel <= MAX_LEVEL)
         {
             Split();
-
             for (int i = 0; i < mDataList.Count;)
             {
-                node = GetNode(data.mRect);
+                node = GetNode(mDataList[i].mRect);
                 if (node != null)
                 {
                     node.Insert(mDataList[i]);
@@ -137,17 +153,80 @@ public class QuadTreeNode<T> where T : Bound
         }
     }
 
-    public void Retrieve(ref List<T> list, QuadTreeNode<T> node, Rect rect)
+    public void Retrieve(ref List<T> list, Rect rect)
     {
-        QuadTreeNode<T> nextNode = node.GetNode(rect);
+        QuadTreeNode<T> nextNode = GetNode(rect);
         if (nextNode != null)
         {
-            list.AddRange(node.mDataList);
-            Retrieve(ref list, nextNode, mRect);
+            list.AddRange(mDataList);
+            nextNode.Retrieve(ref list, mRect);
         }
         else
         {
-            GetNodeAllData(list, node);
+            GetNodeAllData(ref list);
+        }
+    }
+
+    public void Refresh(QuadTreeNode<T> root)
+    {
+        for(int i = 0; i < mDataList.Count;)
+        {
+            QuadTreeNode<T> nextNode = GetNode(mDataList[i].mRect);
+            if(nextNode == null)
+            {
+                root.Insert(mDataList[i]);
+                mDataList.RemoveAt(i);
+            }
+            else
+            {
+                i++;
+            }
+        }
+
+        if(mNodeList != null)
+        {
+            for (int i = 0; i < mNodeList.Length; i++)
+            {
+                mNodeList[i].Refresh(root);
+            }
+        }
+    }
+
+    private void DrawRegion(Rect rect)
+    {
+        Vector3 from = new Vector3(rect.x, 0.0f, rect.y);
+        Vector3 to = new Vector3(rect.x + rect.width, 0.0f, rect.y);
+        Gizmos.DrawLine(from, to);
+
+        from = new Vector3(rect.x + rect.width, 0.0f, rect.y);
+        to = new Vector3(rect.x + rect.width, 0.0f, rect.y + rect.height);
+        Gizmos.DrawLine(from, to);
+
+        from = new Vector3(rect.x + rect.width, 0.0f, rect.y + rect.height);
+        to = new Vector3(rect.x, 0.0f, rect.y + rect.height);
+        Gizmos.DrawLine(from, to);
+
+        from = new Vector3(rect.x, 0.0f, rect.y + rect.height);
+        to = new Vector3(rect.x, 0.0f, rect.y);
+        Gizmos.DrawLine(from, to);
+    }
+
+    public void DrawAll()
+    {
+        Gizmos.color = Color.black;
+        for (int i = 0; i < mDataList.Count; i++)
+        {
+            DrawRegion(mDataList[i].mRect);
+        }
+        Gizmos.color = Color.white;
+        DrawRegion(mRect);
+
+        if(mNodeList != null)
+        {
+            for (int i = 0; i < mNodeList.Length; i++)
+            {
+                mNodeList[i].DrawAll();
+            }
         }
     }
 }
