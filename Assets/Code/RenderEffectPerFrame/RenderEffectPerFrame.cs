99af9c4e81aa0149225113f76043522f8aed33cf
Monk <as13679019999@qq.com>
2019-02-15 18:04:54 +0800

    提交

---
+++ Assets/Code/RenderEffectPerFrame/RenderEffectPerFrame.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderEffectPerFrame : MonoBehaviour {
+
+    public Camera camera;
+    public int width;
+    public int height;
+    public int i = 0;
+
+	// Use this for initialization
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(i < 30)
+        {
+            i++;
+            camera.targetTexture = new RenderTexture(width, height, 24);
+            camera.Render();
+            TextureUtility.SaveRenderTexture(camera.targetTexture, width, height, TextureUtility.TexFormat.ePNG, Application.dataPath + "/TextureSave/" + i + ".png");
+        }
+	}
+}
---
+++ Assets/Code/Utility/TextureUtility.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class TextureUtility : MonoBehaviour {
+
+    public enum TexFormat
+    {
+        ePNG,
+        eJPG,
+    }
+
+	public static Texture2D ConvertRenderTextureToTexture2D(RenderTexture rt, int width, int height)
+    {
+        RenderTexture.active = rt;
+        Texture2D texture2D = new Texture2D(width, height, TextureFormat.ARGB32, false);
+        texture2D.ReadPixels(new Rect(0, 0, width, height), 0, 0);
+        texture2D.Apply();
+        return texture2D;
+    }
+
+    public static void SaveRenderTexture(RenderTexture rt, int width, int height, TexFormat format, string path)
+    {
+        Texture2D texture = ConvertRenderTextureToTexture2D(rt, width, height);
+        if(texture != null)
+        {
+            byte[] textureData = null;
+            switch(format)
+            {
+                case TexFormat.eJPG:
+                    {
+                        textureData = texture.EncodeToJPG();
+                    }
+                    break;
+                case TexFormat.ePNG:
+                    {
+                        textureData = texture.EncodeToPNG();
+                    }
+                    break;
+            }
+
+            FileStream fs = File.Open(path, FileMode.Create, FileAccess.ReadWrite);
+            BinaryWriter bw = new BinaryWriter(fs);
+            bw.Write(textureData);
+            bw.Close();
+            fs.Close();
+            fs.Dispose();
+        }
+    }
+}
